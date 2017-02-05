f0877ceadb1915610c0d8ccd71c8653c70620246
thevenba <baptiste.thevenet@iut-valence.fr>
2017-02-05 22:00:18 +0100

    2nd commit

--- index.php
+++ index.php
@@ -8,70 +8,153 @@ and open the template in the editor.
     <head>
         <meta charset="UTF-8">
         <title></title>
-        <script src="script/parallax.js"></script>
-        <script src="script/requestAnimationFrame.js"></script>
-        <style>
-            html, body, ul, li, img {
-                margin: 0;
-            }
-
-            ul {
-                list-style: none;
-                display: block;
-                padding: 0;
-                margin: 0;
-            }
-
-            #wrapper {
-                position: absolute;
-                overflow: hidden;
-                display: table;
-                height: 100%;
-                width: 100%;
-                left: 0;
-                top: 0;
-            }
-
-            #scene {
-                position: relative;
-                overflow: hidden;
-                display: block;
-                height: 100%;
-                width: 100%;
-                padding: 0;
-                margin: 0;
-            }
-
-            .layer {
-                height: 100%;
-                width: 100%;
-                padding: 0;
-                margin: 0;
-            }
-
-            .layer > div {
-                height: 100%;
-                background-size: cover;
-            }
-        </style>
+        <link rel="stylesheet" href="css/styles.css"/>
+        <link rel="stylesheet" href="css/projects.css"/>
+        <link href="https://fonts.googleapis.com/css?family=Kaushan+Script" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css?family=Della+Respira" rel="stylesheet">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+        <script src="js/jquery.parallax-1.1.3.js"></script>
+        <script src="js/parallax.js"></script>
+        <script src="js/requestAnimationFrame.js"></script>
+        <script src="https://use.fontawesome.com/ed61d7741c.js"></script>
     </head>
     <body>
         <div id="wrapper">
             <ul id="scene" >
-                <li class="layer" data-depth="0.00"><div style="background-image: url(img/back1.jpg);"></div></li>
-                <li class="layer" data-depth="0.40"><div style="background-image: url(img/back2.png);"></div></li>
-                <li class="layer" data-depth="0.80"><div style="background-image: url(img/back3.png);"></div></li>
-                <li class="layer" data-depth="0.60"><div style="background-image: url(img/back4.png);"></div></li>
-                <li class="layer" data-depth="1.00"><div style="background-image: url(img/back5.png);"></div></li>
-                <li class="layer" data-depth="0.60"><div style="background-image: url(img/baptiste.png);"></div></li>
-                <li class="layer" data-depth="0.40"><div style="background-image: url(img/thevenet.png);"></div></li>
-                <li class="layer" data-depth="0.30"><div style="background-image: url(img/webdesigner.png);"></div></li>
-                <li class="layer" data-depth="0.20"><div style="background-image: url(img/developpeur-web.png);"></div></li>
+                <li class="layer" data-depth="0.00"><div class="bg" style="background-image: url(img/background1/back1.jpg);"></div></li>
+                <li class="layer" data-depth="0.20"><div class="bg" style="background-image: url(img/background1/back2.png);"></div></li>
+                <li class="layer" data-depth="0.50"><div class="bg" style="background-image: url(img/background1/back3.png);"></div></li>
+                <li class="layer" data-depth="0.55"><div class="bg" style="background-image: url(img/background1/back4.png);"></div></li>
+                <li class="layer" data-depth="0.80"><div class="bg" style="background-image: url(img/background1/back5.png);"></div></li>
+                <li class="layer" data-depth="-0.60"><div class="bg" style="background-image: url(img/background1/baptiste.png);"></div></li>
+                <li class="layer" data-depth="0.40"><div class="bg" style="background-image: url(img/background1/thevenet.png);"></div></li>
+                <li class="layer" data-depth="0.30"><div class="bg" style="background-image: url(img/background1/webdesigner.png);"></div></li>
+                <li class="layer" data-depth="-0.20"><div class="bg" style="background-image: url(img/background1/developpeur-web.png);"></div></li>
             </ul>
+        </div>
+        <div id="sky" class="bg" style="background-image: url(img/background2/sky.jpg)">
+            <div id="sun" class="bg" style="background-image: url(img/background2/sun2.png)">
+                <div id="back" class="bg" style="background-image: url(img/background2/back.png)">
+                    <div id="front" class="bg" style="background-image: url(img/background2/front.png)">
+                        <div id="particles" class="bg" style="background-image: url(img/background2/particles.png)">
+                            <div id="refugePlanAiguille" class="project">
+                                <img src="img/refuge-plan-aiguille.jpg"/>
+                                <div class="overlay">
+                                    <div>
+                                        <h2>Refuge du Plan de l'aiguille</h2>
+                                        <p>2016 <span style="color: #b25968">&#9899;</span> Site Web</p>
+                                        <div class="project-content">
+
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div id="lastSection" class="row">
+            <div id="about" class="col half">
+                <div>
+                    <h2>À propos de moi</h2>
+                    <p>
+                        Mon nom est Baptiste Thevenet, je suis Webdesigner et 
+                        développeur Web. Actuellement en licence professionelle 
+                        Webdesign à l'Institut Universitaire de Mulhouse, je 
+                        possède déjà un diplôme universitaire de technologie en 
+                        Informatique. Ces deux cursus me permettent ainsi d'être
+                        très polyvalent dans le domaine du Web.
+                    </p>
+                    <div class="about-links">
+                        <a href='http://www.viadeo.com/p/002ygoymlm75p9b'><i class="fa fa-viadeo fa-2x fa-fw"></i></a>
+                        <a href='mailto:hello@baptistethevenet.com'><i class="fa fa-envelope fa-2x fa-fw"></i></a>
+                    </div>
+                </div>
+            </div>
+            <div id="contact" class="col half">
+                <div>
+                    <h2>Contactez moi</h2>
+                    <form>
+                        <div class="input-group">
+                            <label>Nom*</label>
+                            <div class="input">
+                                <input type="text" name="name" required/>
+                            </div>
+                        </div>
+                        <div class="input-group">
+                            <label>Email*</label>
+                            <div class="input">
+                                <input type="email" name="email" required/>
+                            </div>
+                        </div>
+                        <div class="input-group">
+                            <label>Message*</label>
+                            <div class="input">
+                                <textarea name="message" required></textarea>
+                            </div>
+                        </div>
+                        <div class="input-group">
+                            <button type="submit" name="submit"><span><i class="fa fa-long-arrow-right"></i>Envoyer</span></button>
+                        </div>
+                    </form>
+                </div>
+            </div>
         </div>
     </body>
     <script>
-        var scene = document.getElementById('scene');
-        var parallax = new Parallax(scene);
+        $(document).ready(function () {
+            $(window).scroll(function () {
+                if ($(document).scrollTop() < $("#sky").offset().top + $('#sky').offset().top / 2) {
+                    $("#about > div").addClass("hidden");
+                    $("#contact > div").addClass("hidden");
+                } else {
+                    $("#about > div").removeClass("hidden");
+                    $("#contact > div").removeClass("hidden");
+                }
+            });
+
+
+            var scene = document.getElementById('scene');
+            var parallax = new Parallax(scene);
+
+            $('#sky').parallax("center", 0.1, true);
+            $('#back').parallax("center", 0.3, true);
+            $('#front').parallax("center", 0.5, true);
+            $('#sun').parallax("center", -0.1, true);
+            $('#particles').parallax("center", -0.5, true);
+
+            $('.project').each(function () {
+                $(this).click(function () {
+                    var id = $(this).attr("id");
+//                    $('#' + id).css("width", "100%").css("margin", "0");
+//                    $('#' + id + ' .overlay').css("width", "100%");
+                    $('#' + id).animate({width: "100%"}).animate({margin: "0"});
+                    $('#' + id + ' .overlay').css("width", "100%").css("backgroundColor", "rgba(255,255,255,1)");
+                    $('#' + id + ' .overlay > div').delay(1000).animate({top: "-10%"});
+                    myFunction(id);
+
+                });
+            });
+
+            function myFunction(id) {
+                setTimeout(openProject, 1500);
+
+                function openProject() {
+                    var xhttp = new XMLHttpRequest();
+                    xhttp.onreadystatechange = function () {
+                        if (this.readyState == 4 && this.status == 200) {
+//                            $('#' + id + ' .project-content').html(this.responseText);
+                            $('body').html(this.responseText);
+                        }
+                    };
+                    xhttp.open("GET", "./projects/" + id + ".php", true);
+                    xhttp.send();
+                }
+            }
+
+
+        });
+
     </script>
 </html>
---
+++ projects/refugePlanAiguille.php
@@ -0,0 +1,11 @@
+<div class="project-wrapper">
+    <a href="./index.php" class="back-top-index"><i class="fa fa-long-arrow-left fa-2x"></i></a>
+    <div class="banner">
+        <h2>Refuge du Plan de l'Aiguille</h2>
+        <p class="info">2016 <span style="color: #b25968">&#9899;</span> Site Web</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed malesuada, justo a scelerisque porttitor, turpis libero interdum justo, eu aliquam lacus est vel velit. Nulla facilisi. Nunc quis enim lacinia, lacinia velit at, porttitor nibh. Curabitur lobortis, metus sed vestibulum ornare, tellus nisl maximus est, eget pulvinar ligula nulla ut quam. Donec sit amet lorem commodo, dictum ante nec, placerat ligula. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Vivamus tellus enim, gravida quis nisl ut, commodo molestie enim. Nam non dignissim erat. Praesent fermentum molestie eleifend.</p>
+    </div>
+    <div class="project-img">
+        <a href='http://refuge-plan-aiguille.com'><img src="./img/refuge-plan-aiguille.jpg"/></a>
+    </div>
+</div>
